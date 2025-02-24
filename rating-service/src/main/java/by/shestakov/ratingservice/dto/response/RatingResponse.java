5ccef90fef4e4018b4d435f85a1e5e474497b2b0
Froxy152 <rukyss.games.91@mail.ru>
2025-02-24 12:19:15 +0300

    fix code style

--- rating-service/src/main/java/by/shestakov/ratingservice/dto/response/RatingResponse.java
+++ rating-service/src/main/java/by/shestakov/ratingservice/dto/response/RatingResponse.java
@@ -5,17 +5,11 @@ import by.shestakov.ratingservice.entity.RatedBy;
 
 public record RatingResponse(
         String id,
-
         String rideId,
-
         Long passengerId,
-
         Long driverId,
-
         Double rate,
-
         String commentary,
-
         RatedBy ratedBy
 ) {
 }
