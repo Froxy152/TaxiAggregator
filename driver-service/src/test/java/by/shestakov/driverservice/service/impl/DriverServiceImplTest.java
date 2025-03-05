487770c4af429fb58e36ab74fb235f6624705d25
Froxy152 <rukyss.games.91@mail.ru>
2025-03-05 16:26:27 +0300

    part of unit test, delete unused, refactor some code

--- driver-service/src/test/java/by/shestakov/driverservice/service/impl/DriverServiceImplTest.java
+++ driver-service/src/test/java/by/shestakov/driverservice/service/impl/DriverServiceImplTest.java
@@ -4,24 +4,32 @@ import static by.shestakov.constant.TestDriverData.TEST_ID;
 import static by.shestakov.constant.TestDriverData.defaultDriver;
 import static by.shestakov.constant.TestDriverData.defaultDriverRequest;
 import static by.shestakov.constant.TestDriverData.defaultDriverResponse;
+import static by.shestakov.constant.TestDriverData.driverUpdateRequest;
+import static by.shestakov.constant.TestDriverData.updatedDriver;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import by.shestakov.driverservice.dto.request.DriverRequest;
+import by.shestakov.driverservice.dto.request.DriverUpdateRequest;
 import by.shestakov.driverservice.dto.response.DriverResponse;
 import by.shestakov.driverservice.entity.Driver;
+import by.shestakov.driverservice.exception.driver.DriverAlreadyExistsException;
 import by.shestakov.driverservice.exception.driver.DriverNotFoundException;
 import by.shestakov.driverservice.mapper.DriverMapper;
 import by.shestakov.driverservice.repository.DriverRepository;
+import by.shestakov.driverservice.util.ExceptionMessages;
 import jakarta.persistence.Id;
+import java.math.BigDecimal;
 import java.util.Optional;
+import org.aspectj.apache.bcel.ExceptionConstants;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.util.Assert;
 
 @ExtendWith(MockitoExtension.class)
 class DriverServiceImplTest {
@@ -36,15 +44,8 @@ class DriverServiceImplTest {
     @InjectMocks
     private DriverServiceImpl driverService;
 
-    @BeforeEach
-    void setUp() {
-
-    }
-
     @Test
-    void getAllDrivers() {
-
-
+    void getAllDrivers() { //todo test for page
     }
 
     @Test
@@ -83,18 +84,144 @@ class DriverServiceImplTest {
             DriverNotFoundException.class,
             () -> driverService.getById(id));
 
+        assertEquals(ExceptionMessages.NOT_FOUND_MESSAGE.formatted("driver", id), exception.getMessage());
+
         verify(driverRepository).findByIdAndIsDeletedFalse(id);
     }
 
     @Test
-    void createDriver() {
+    void createDriver_ReturnsValidResponse() {
+        DriverRequest request = defaultDriverRequest();
+        Driver driver = defaultDriver();
+        DriverResponse expectedResponse = defaultDriverResponse();
+
+        when(driverRepository.existsByEmailOrPhoneNumber(request.email(), request.phoneNumber())).thenReturn(false);
+        when(driverMapper.toEntity(request)).thenReturn(driver);
+        when(driverRepository.save(driver)).thenReturn(driver);
+        when(driverMapper.toDto(driver)).thenReturn(expectedResponse);
+
+        DriverResponse response = driverService.createDriver(request);
+
+        assertNotNull(response);
+        assertEquals(expectedResponse.id(), response.id());
+        assertEquals(expectedResponse.name(), response.name());
+        assertEquals(expectedResponse.lastName(), response.lastName());
+        assertEquals(expectedResponse.email(), response.email());
+        assertEquals(expectedResponse.phoneNumber(), response.phoneNumber());
+        assertEquals(expectedResponse.gender(), response.gender());
+        assertEquals(expectedResponse.rating(), response.rating());
+        assertEquals(expectedResponse.cars(), response.cars());
+        assertEquals(BigDecimal.valueOf(0.0), driver.getRating());
+        assertFalse(driver.getIsDeleted());
+
+
+        verify(driverRepository).existsByEmailOrPhoneNumber(request.email(), request.phoneNumber());
+        verify(driverMapper).toEntity(request);
+        verify(driverRepository).save(driver);
+        verify(driverMapper).toDto(driver);
     }
 
     @Test
-    void updateDriver() {
+    void createDriver_DriverExists_ThrowException() {
+        DriverRequest request = defaultDriverRequest();
+
+        when(driverRepository.existsByEmailOrPhoneNumber(request.email(), request.phoneNumber())).thenReturn(true);
+
+        DriverAlreadyExistsException exception = assertThrows(
+            DriverAlreadyExistsException.class,
+            () -> driverService.createDriver(request)
+        );
+
+        assertEquals(ExceptionMessages.CONFLICT_MESSAGE.formatted("driver"), exception.getMessage());
+
+        verify(driverRepository).existsByEmailOrPhoneNumber(request.email(), request.phoneNumber());
     }
 
     @Test
-    void deleteDriver() {
+    void updateDriver_ReturnsValidResponse() {
+        Long id = TEST_ID;
+        Driver driver = defaultDriver();
+        DriverUpdateRequest request = driverUpdateRequest();
+        DriverResponse expectedResponse = updatedDriver();
+
+        when(driverRepository.findByIdAndIsDeletedFalse(id)).thenReturn(Optional.of(driver));
+        when(driverRepository.existsByEmailOrPhoneNumber(request.email(), request.phoneNumber()))
+            .thenReturn(false);
+        when(driverMapper.toDto(driver)).thenReturn(expectedResponse);
+
+        DriverResponse response = driverService.updateDriver(request, id);
+
+        assertNotNull(response);
+        assertEquals(expectedResponse, response);
+
+        verify(driverRepository).findByIdAndIsDeletedFalse(id);
+        verify(driverRepository).existsByEmailOrPhoneNumber(request.email(), request.phoneNumber());
+        verify(driverMapper).updateToExists(request, driver);
+        verify(driverRepository).save(driver);
+    }
+
+    @Test
+    void updateDriver_NotFoundDriver_ThrowException() {
+        Long id = TEST_ID;
+        DriverUpdateRequest request = driverUpdateRequest();
+
+        when(driverRepository.findByIdAndIsDeletedFalse(id)).thenReturn(Optional.empty());
+
+        DriverNotFoundException exception = assertThrows(
+            DriverNotFoundException.class,
+            () -> driverService.updateDriver(request, id));
+
+        assertEquals(ExceptionMessages.NOT_FOUND_MESSAGE.formatted("driver", id), exception.getMessage());
+
+        verify(driverRepository).findByIdAndIsDeletedFalse(id);
+    }
+
+    @Test
+    void updateDriver_DriverExists_ThrowException() {
+        Long id = TEST_ID;
+        DriverUpdateRequest request = driverUpdateRequest();
+        Driver driver = defaultDriver();
+
+        when(driverRepository.findByIdAndIsDeletedFalse(id)).thenReturn(Optional.of(driver));
+        when(driverRepository.existsByEmailOrPhoneNumber(request.email(), request.phoneNumber())).thenReturn(true);
+
+        DriverAlreadyExistsException exception = assertThrows(
+            DriverAlreadyExistsException.class,
+            () -> driverService.updateDriver(request, id)
+        );
+
+        assertEquals(ExceptionMessages.CONFLICT_MESSAGE.formatted("driver"), exception.getMessage());
+
+        verify(driverRepository).findByIdAndIsDeletedFalse(id);
+        verify(driverRepository).existsByEmailOrPhoneNumber(request.email(), request.phoneNumber());
+    }
+
+    @Test
+    void deleteDriver_ReturnsValidDeleted() {
+        Long id = TEST_ID;
+        Driver driver = defaultDriver();
+
+        when(driverRepository.findByIdAndIsDeletedFalse(id)).thenReturn(Optional.of(driver));
+
+        driverService.deleteDriver(id);
+
+        assertTrue(driver.getIsDeleted());
+
+        verify(driverRepository).findByIdAndIsDeletedFalse(id);
+        verify(driverRepository).save(driver);
+    }
+
+    @Test
+    void deleteDriver_DriverNotFound_ThrowException() {
+        Long id = TEST_ID;
+
+        when(driverRepository.findByIdAndIsDeletedFalse(id)).thenReturn(Optional.empty());
+
+        DriverNotFoundException exception = assertThrows(
+            DriverNotFoundException.class,
+            ()  -> driverService.deleteDriver(id)
+        );
+
+        assertEquals(ExceptionMessages.NOT_FOUND_MESSAGE.formatted("driver", id), exception.getMessage());
     }
 }
