81ab3a73e56f9c4fb26258f9743d791d8c3fda57
Froxy152 <rukyss.games.91@mail.ru>
2025-04-02 13:49:50 +0300

    add elk compose

--- rides-service/src/main/java/by/shestakov/ridesservice/feign/DriverClient.java
+++ rides-service/src/main/java/by/shestakov/ridesservice/feign/DriverClient.java
@@ -1,15 +1,12 @@
 package by.shestakov.ridesservice.feign;
 
 import static by.shestakov.ridesservice.util.constant.ExceptionMessage.DRIVER_SERVICE_UNAVAILABLE_CIRCUIT_BREAKER;
-import static by.shestakov.ridesservice.util.constant.ExceptionMessage.PASSENGER_SERVICE_UNAVAILABLE_CIRCUIT_BREAKER;
 
 import by.shestakov.ridesservice.dto.response.DriverResponse;
 import by.shestakov.ridesservice.exception.CustomErrorDecoder;
 import by.shestakov.ridesservice.exception.FallbackException;
-import by.shestakov.ridesservice.exception.FeignNotFoundDataException;
 import feign.FeignException;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +22,7 @@ public interface DriverClient {
         if (throwable instanceof FeignException feignClientException) {
             throw new FallbackException(DRIVER_SERVICE_UNAVAILABLE_CIRCUIT_BREAKER);
         }
-        throw new FeignNotFoundDataException(throwable.getMessage());
+        return null;
+       // throw new FeignNotFoundDataException(throwable.getMessage());
     }
 }
